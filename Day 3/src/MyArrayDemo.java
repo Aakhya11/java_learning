b39c97227f8e179a67edf883ab62b43fffab5e48
Aakhya Singh <aakhyasingh07@gmail.com>
2025-09-15 23:18:17 +0530

    arrays

---
+++ Day 3/src/MyArrayDemo.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class MyArrayDemo {
+    public static void main(String[] args){
+        int[] a;
+        a= new int[3];
+        a[0]=10;
+        a[1]=20;
+        a[2]=30;
+        System.out.println("value of a[0] is "+ a[0]);
+    }
+
+}
+
+
+
