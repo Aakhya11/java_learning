8f3899f5e65916c2dedbd7ec96f078a2fd95ffdd
Aakhya Singh <aakhyasingh07@gmail.com>
2025-09-25 22:31:32 +0530

    Static import

---
+++ Day9/src/Main.java
@@ -0,0 +1,23 @@
+
+
+/
+//NON-STATIC IMPORT;
+class Test
+{
+    public static void main(String args[]){
+        System.out.println(Math.sqrt(4));
+        System.out.println(Math.max(10,20));
+        System.out.println(Math.random());
+    }}
+
+//STATIC IMPORT;
+//import static java.lang.Math.sqrt;
+//import static java.lang.Math.*;
+//
+//class Test
+//{
+//    public static void main(String args[]){
+//        System.out.println(sqrt(4));
+//        System.out.println(max(10,20));
+//        System.out.println(random());
+//    }}
